a90879c27356b6dea74a60ef63da5ac2863c54fa
rachel.chuang <rachel.chuang@androvideo.com>
2020-09-14 11:53:50 +0800

    1. reformat the code 2. bind light icon to NO/NC

--- app/src/main/java/com/avc/app/gpiodemo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/avc/app/gpiodemo/ui/main/MainViewModel.kt
@@ -10,109 +10,98 @@ import com.avc.app.gpiodemo.R
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
     private val _isDiEnabled = MutableLiveData<Boolean>()
-
     val isDiEnabled: LiveData<Boolean>
         get() = _isDiEnabled
-
     fun clickDiSwitch() {
         _isDiEnabled.value = (_isDiEnabled.value == false)
     }
 
-    private val _isDo1Enabled = MutableLiveData<Boolean>()
 
+    // DO1
+    private val _isDo1Enabled = MutableLiveData<Boolean>()
     val isDo1Enabled: LiveData<Boolean>
         get() = _isDo1Enabled
-
     fun clickDo1Switch() {
         _isDo1Enabled.value = (_isDo1Enabled.value == false)
     }
 
-    private val _isDo2Enabled = MutableLiveData<Boolean>()
-
-    val isDo2Enabled: LiveData<Boolean>
-        get() = _isDo2Enabled
-
-    fun clickDo2Switch() {
-        _isDo2Enabled.value = (_isDo2Enabled.value == false)
-    }
+    private val _isDO1NormalOpen = MutableLiveData<Boolean>()
+    val isDO1NormalOpen: LiveData<Boolean>
+        get() = _isDO1NormalOpen
 
     private val _do1PulseSec = MutableLiveData<Int>()
-
     val do1PulseSec: LiveData<Int>
         get() = _do1PulseSec
-
     fun changeDo1PulseTime(sec: Int) {
         _do1PulseSec.value = sec
     }
 
     private val _do1DelaySec = MutableLiveData<Int>()
-
     val do1DelaySec: LiveData<Int>
         get() = _do1DelaySec
-
     fun changeDo1DelayTime(sec: Int) {
         _do1DelaySec.value = sec
     }
 
-    private val _do2PulseSec = MutableLiveData<Int>()
+    private val _isDO1TriggeredTooOften = MutableLiveData<Boolean>()
+    val isDO1TriggeredTooOften: LiveData<Boolean>
+        get() = _isDO1TriggeredTooOften
 
+    private var do1LastTimeTriggered: Long = 0
+
+    private val _isDO1LightOn = MutableLiveData<Boolean>()
+    val isDO1LightOn: LiveData<Boolean>
+        get() = _isDO1LightOn
+    fun changeDO1Light(isNormalOpen: Boolean){
+        _isDO1LightOn.value = isNormalOpen
+    }
+
+    //DO2
+    private val _isDo2Enabled = MutableLiveData<Boolean>()
+    val isDo2Enabled: LiveData<Boolean>
+        get() = _isDo2Enabled
+    fun clickDo2Switch() {
+        _isDo2Enabled.value = (_isDo2Enabled.value == false)
+    }
+
+    private val _isDO2NormalOpen = MutableLiveData<Boolean>()
+    val isDO2NormalOpen: LiveData<Boolean>
+        get() = _isDO2NormalOpen
+
+    private val _do2PulseSec = MutableLiveData<Int>()
     val do2PulseSec: LiveData<Int>
         get() = _do2PulseSec
-
     fun changeDo2PulseTime(sec: Int) {
         _do2PulseSec.value = sec
     }
 
     private val _do2DelaySec = MutableLiveData<Int>()
-
     val do2DelaySec: LiveData<Int>
         get() = _do2DelaySec
-
     fun changeDo2DelayTime(sec: Int) {
         _do2DelaySec.value = sec
     }
 
-    private val _seekBarDisplayValue = MutableLiveData<Int>()
-
-    val seekBarDisplayValue: LiveData<Int>
-        get() = _seekBarDisplayValue
-
-    fun changeSeekBarDisplay(sec: Int) {
-        _seekBarDisplayValue.value = sec
-    }
-
-    var do1LastTimeTriggered: Long = 0
-    var do2LastTimeTriggered: Long = 0
-
-    private val _isDO1TriggeredTooOften = MutableLiveData<Boolean>()
-
-    val isDO1TriggeredTooOften: LiveData<Boolean>
-        get() = _isDO1TriggeredTooOften
-
     private val _isDO2TriggeredTooOften = MutableLiveData<Boolean>()
-
     val isDO2TriggeredTooOften: LiveData<Boolean>
         get() = _isDO2TriggeredTooOften
 
-    private val _isDO1NormalOpen = MutableLiveData<Boolean>()
-
-    val isDO1NormalOpen: LiveData<Boolean>
-        get() = _isDO1NormalOpen
-
-    private val _isDO2NormalOpen = MutableLiveData<Boolean>()
-
-    val isDO2NormalOpen: LiveData<Boolean>
-        get() = _isDO2NormalOpen
-
-    private val _isDO1LightOn = MutableLiveData<Boolean>()
-
-    val isDO1LightOn: LiveData<Boolean>
-        get() = _isDO1LightOn
+    private var do2LastTimeTriggered: Long = 0
 
     private val _isDO2LightOn = MutableLiveData<Boolean>()
-
     val isDO2LightOn: LiveData<Boolean>
         get() = _isDO2LightOn
+    fun changeDO2Light(isNormalOpen: Boolean){
+        _isDO2LightOn.value = isNormalOpen
+    }
+
+
+    private val _seekBarDisplayValue = MutableLiveData<Int>()
+    val seekBarDisplayValue: LiveData<Int>
+        get() = _seekBarDisplayValue
+    fun changeSeekBarDisplay(sec: Int) {
+        _seekBarDisplayValue.value = sec
+    }
 
     init {
         _isDiEnabled.value = false
@@ -124,6 +113,8 @@ class MainViewModel : ViewModel() {
         _do2DelaySec.value = 1
         _isDO1TriggeredTooOften.value = false
         _isDO2TriggeredTooOften.value = false
+        _isDO1LightOn.value = true
+        _isDO2LightOn.value = true
     }
 
     fun triggerEvent() {
